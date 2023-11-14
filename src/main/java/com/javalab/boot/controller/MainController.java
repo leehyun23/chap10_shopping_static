ac7d8ea781a28ef63a844c94c9e1bcd4ffce9699
leehyun23 <gkdistjd12@naver.com>
2023-11-14 10:48:56 +0900

    Main화면 수정

--- src/main/java/com/javalab/boot/controller/MainController.java
+++ src/main/java/com/javalab/boot/controller/MainController.java
@@ -12,7 +12,7 @@ import java.util.Arrays;
 import java.util.List;
 
 /**
- 메인 화면용 컨트롤러
+ * 메인 화면용 컨트롤러
  */
 @Controller
 @RequiredArgsConstructor
@@ -23,24 +23,24 @@ public class MainController {
     @GetMapping("/")
     public String list(@ModelAttribute("pageRequestDTO") PageRequestDTO pageRequestDTO,
                        @ModelAttribute("itemSearchDto") ItemSearchDto itemSearchDto,
-                       Model model){
+                       Model model) {
 
         PageResponseDTO<MainItemDto> responseDTO = itemService.searchMainPage(pageRequestDTO, itemSearchDto);
         List<MainItemDto> items = responseDTO.getDtoList();
 
         // 슬라이드 정보 세팅
         List<SlideDto> slides = Arrays.asList(
-                new SlideDto("images/main01.jpg", "  ", "NEW SEASON", "/item/list"),
-                new SlideDto("images/main02.jpg", "2023 Men New-Season Autumn", "Jackets & Coats",  "/item/list"),
-                new SlideDto("images/main03.jpg", "2023 Men Collection Winter", "New arrivals",  "/item/list"),
-                new SlideDto("images/main04.jpg", "2023 Men Collection Winter", "New arrivals",  "/item/list"),
-                new SlideDto("images/main05.jpg", "2023 ", "New",  "/item/list"),
-                new SlideDto("images/main06.jpg", "2023 ", "New",  "/item/list")
-        );
 
-        model.addAttribute("responseDTO", responseDTO);
-        model.addAttribute("items", items);
-        model.addAttribute("maxPage", 5); // 나중에 수정
+                new SlideDto("images/main01.jpg", "  ", " ", "/item/list"),
+                new SlideDto("images/main02.jpg", " ", " ", "/item/list"),
+                new SlideDto("images/main03.jpg", " ", " ", "/item/list"),
+                new SlideDto("images/main04.jpg", " ", " ", "/item/list"),
+                new SlideDto("images/main05.jpg", "  ", " ", "/item/list")
+                );
+
+                model.addAttribute("responseDTO", responseDTO);
+                model.addAttribute("items", items);
+                model.addAttribute("maxPage", 5); // 나중에 수정
         model.addAttribute("slides", slides); // 슬라이드
 
         return "main";
